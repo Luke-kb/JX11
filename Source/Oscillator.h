ef0b335aca9920e073c19a7d423b563832d5cdda
Luke KB <>
2022-10-14 21:12:47 +1100

    simple band-limited impulse train implementation

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -10,56 +10,59 @@
 
 #pragma once
 
+#include <cmath>
+
+const float PI_OVER_4 = 0.7853981633974483f;
+const float PI = 3.1415926535897932f;
 const float TWO_PI = 6.2831853071795864f;
 
 class Oscillator
 {
 public:
-    float amplitude;
-    float inc;  // increment
-    float phase;
-    float freq;
-    float sampleRate;
-    float phaseBL; // band-limited phase
+    float period = 0.0f;
+    float amplitude = 1.0f;
     
     void reset()
     {
+        inc = 0.0f;
         phase = 0.0f;
-        phaseBL = -0.5f;
-    }
-    
-    float nextBandLimitedSample()
-    {
-        phaseBL += inc;
-        if (phaseBL >= 1.0f) {
-            phaseBL -= 1.0f;
-        }
-        
-        float output = 0.0f;
-        float nyquist = sampleRate / 2.0f;
-        float h = freq;
-        float i = 1.0f;
-        float m = 0.6366197724f; // aka 2/pi
-        
-        while (h < nyquist) {
-            output += m * std::sin(TWO_PI * phaseBL * i) / i;
-            h += freq;
-            i += 1.0f;
-            m = -m;
-        }
-        return output;
     }
     
     float nextSample()
     {
-        // just the aliases :/
+        float output = 0.0f;
+        
         phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
+        
+        if (phase <= PI_OVER_4) {
+            
+            float halfPeriod = period / 2.0f; // midpoint between current peak and the next one
+            
+            phaseMax = std::floor(0.5f + halfPeriod) - 0.5f;
+            phaseMax *= PI;
+            
+            inc = phaseMax / halfPeriod;
+            phase = -phase;
+            
+            if ((phase * phase) > 1e-9) { // aka: if (phase < -1e-9 || phase > 1e-9)
+                output = (amplitude * std::sin(phase)) / phase;
+            } else {
+                output = amplitude;
+            }
+        } else {
+            if (phase > phaseMax) {
+                phase = (phaseMax + phaseMax) - phase;
+                inc = -inc;
+            }
+            
+            output = (amplitude * std::sin(phase)) / phase;
         }
         
-        float aliased = 2.0f * phase - 1.0f;
-        
-        return amplitude * (aliased - nextBandLimitedSample());
+        return output;
     }
+    
+private:
+    float phase; // measured as: sample * PI
+    float phaseMax; // measured as: sample * PI
+    float inc;
 };
--- Source/Synth.cpp
+++ Source/Synth.cpp
@@ -56,13 +56,10 @@ void Synth::noteOn(int note, int velocity)
 {
     voice.note = note;
     
+    float freq = 440.f * std::exp2(float(note - 69) / 12.f); // convert midi to freq
     
-    float freq = 440.f * std::exp2(float(note - 69) / 12.f);
-    
-    voice.osc.freq = freq;
-    voice.osc.sampleRate = sampleRate;
     voice.osc.amplitude = (velocity / 127.0f) * 0.5f;
-    voice.osc.inc = freq / sampleRate;
+    voice.osc.period = sampleRate / freq;
     voice.osc.reset();
 }
 
